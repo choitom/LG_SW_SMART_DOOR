0edc3c6c5154af8ea0ca479cdd2d5f2ca3c2f445
choitom <choito@carleton.edu>
2017-12-06 12:18:57 +0900

    record multiple videos

--- RecordVideo/record.hpp
+++ RecordVideo/record.hpp
@@ -2,7 +2,7 @@
 extern "C"{
 #endif
 
-extern void record_video();
+extern void record_video(int num);
 extern void play_video(const char* title);
 
 #ifdef __cplusplus
--- RecordVideo/test.c
+++ RecordVideo/test.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static int video_num = 0;
+
+void record()
+{
+    record_video(video_num++);
+}
+
 int main(void)
 {
-    record_video();
-    play_video("visitor_recording.avi");
-    printf("DONE\n");
-    return 0;
+	record();
+   	//play_video("visitor_recording0.avi");
+
+    record();
+    //play_video("visitor_recording1.avi");
+	printf("DONE\n");
+	return 0;
 }
